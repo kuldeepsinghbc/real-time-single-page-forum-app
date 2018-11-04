aedcbf6f590a0d460303f20a9d0129b24591d94c
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-11-03 21:55:18 -0700

    Vuetify, Vue Router and Initial Components

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer class="pa-3">
+      Bitfumes
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+
+</style>
